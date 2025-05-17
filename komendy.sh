6e7f8722a15cd81fe713d068df172340ca66da66
Sebix2334 <dsw55988@student.dsw.edu.pl>
2025-05-18 00:59:17 +0200

    zad1

---
+++ komendy.sh
@@ -0,0 +1,22 @@
+git commit
+git commit --amend
+git checkout
+git checkout -b
+git branch
+git branch -f
+git branch -u
+git merge
+git reset
+git revert
+git cherry-pick
+git rebase
+git rebase -i
+git tag
+git describe
+git clone 
+git fetch
+git pull
+git push
+git pull --rebase
+git add
+git commit -m
